7439c707955ac0ec6bb69c3482ae3e3ba6115905
iamchrismolina <chrisdicksonmolina@gmail.com>
2023-10-05 09:56:11 +0800

    feat: add header logo and brand section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+import "./header.scss";
+
 const Header = () => {
-    return (
-        <>
-            <div>Header</div>
-        </>
-    )
-}
+  return (
+    <div className="header">
+      <img
+        className="header__img"
+        src="/public/images/todo-img-pngwing.com.png"
+      />
+      <div className="header__brand">
+        <h1 className="header__title">TODO LIST</h1>
+        <span className="header__desc">Create your list</span>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
