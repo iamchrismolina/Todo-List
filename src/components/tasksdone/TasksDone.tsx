8274fcd2019f4c74be66dc6f0479c8cbe1b29bda
iamchrismolina <ragepool123@gmail.com>
2023-10-21 12:59:07 +0800

    fix: revert commit history

--- src/components/tasksdone/TasksDone.tsx
+++ src/components/tasksdone/TasksDone.tsx
@@ -12,7 +12,7 @@ const TasksDone = ({ tasksCount, setTasksCount }: PropsType) => {
         <div className="tasks__score-title">Tasks Finished</div>
         <div>
           <img
-            src="./public/images/finished-task-icon/finishedtask.jpg"
+            src="./public/images/finished-task-icon/finishedTask.jpg"
             width="100"
             height="100"
             alt=""
